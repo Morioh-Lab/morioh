94e8acd962723e0169d578ca8a70c52edfdc1412
Nasa Nguyen <nasa8x@gmail.com>
2020-02-15 11:30:25 +0700

    update 1.0.4

--- src/js/app.js
+++ src/js/app.js
@@ -18,6 +18,11 @@ $(() => {
     });
 
 
+    new PerfectScrollbar('.perfect-scrollbar', {
+        suppressScrollX: true
+    });
+    
+
     // 
 
     $('#navbar-toggler').on('click', function () {
